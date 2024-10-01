0eee825b583603105af19ec01ed6b71a3832c560
Snxhit_ <74500652+Snxhit@users.noreply.github.com>
2024-10-02 01:02:12 +0530

    create main

---
+++ main.js
@@ -0,0 +1,15 @@
+const { h, render } = window.preact;
+const { useState } = window.preactHooks;
+
+function Game() {
+  const [count, setCount] = useState(0);
+  const handleClick = () => setCount(count + 1);
+
+  return (
+    <div>
+      <h1>Tester Counter</h1>
+      <p>Current Count: {count}</p>
+      <button onClick={handleClick}Increment!</button>
+    </div>
+  );
+}
